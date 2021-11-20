79e11dcf242b361a79bdb222724e26dd6f50b064
linder3hs <linderhassinger00@gmail.com>
2021-11-19 23:40:49 -0500

    Initial Commit

---
+++ iBooks/iBooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  iBooksApp.swift
+//  iBooks
+//
+//  Created by Linder Hassinger on 19/11/21.
+//
+
+import SwiftUI
+
+@main
+struct iBooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
